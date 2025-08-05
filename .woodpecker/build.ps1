199f07727ca1ed074235a8213c7e519d99d2ebc5
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-08-05 12:27:01 +0200

    added manifests for multiplatform push

--- .woodpecker/build.ps1
+++ .woodpecker/build.ps1
@@ -6,9 +6,10 @@ $QUAY_URL = $env:QUAY_URL
 $QUAY_USERNAME = $env:QUAY_USERNAME
 $QUAY_PASSWORD = $env:QUAY_PASSWORD
 
-docker login $DOCKER_URL --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+docker login "$DOCKER_URL" --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 docker login "$QUAY_URL" --username "$QUAY_USERNAME" --password "$QUAY_PASSWORD"
 cd s3cmd
+
 docker build . -f Dockerfile -t opencloudeu/s3cmd:w-v$date
 docker tag opencloudeu/s3cmd:w-v$date opencloudeu/s3cmd:w-v1
 docker tag opencloudeu/s3cmd:w-v$date $DOCKER_URL/opencloudeu/s3cmd:w-v$date
@@ -24,8 +25,10 @@ docker push $QUAY_URL/opencloudeu/s3cmd:w-v$date
 docker push $QUAY_URL/opencloudeu/s3cmd:w-v1
 
 docker manifest create -a opencloudeu/s3cmd:v1 opencloudeu/s3cmd:l-v1 opencloudeu/s3cmd:w-v1
-docker manifest create -a $DOCKER_URL/opencloudeu/s3cmd:v1 $DOCKER_URL/opencloudeu/s3cmd:l-v1 $DOCKER_URL/opencloudeu/s3cmd:w-v1
-docker manifest create -a $QUAY_URL/opencloudeu/s3cmd:v1 $QUAY_URL/opencloudeu/s3cmd:l-v1 $QUAY_URL/opencloudeu/s3cmd:w-v1
 docker manifest push opencloudeu/s3cmd:v1
+
+docker manifest create -a $DOCKER_URL/opencloudeu/s3cmd:v1 $DOCKER_URL/opencloudeu/s3cmd:l-v1 $DOCKER_URL/opencloudeu/s3cmd:w-v1
 docker manifest push $DOCKER_URL/opencloudeu/s3cmd:v1
-docker manifest push $env:QUAY_URL/opencloudeu/s3cmd:v1
+
+docker manifest create -a $QUAY_URL/opencloudeu/s3cmd:v1 $QUAY_URL/opencloudeu/s3cmd:l-v1 $QUAY_URL/opencloudeu/s3cmd:w-v1
+docker manifest push $QUAY_URL/opencloudeu/s3cmd:v1
