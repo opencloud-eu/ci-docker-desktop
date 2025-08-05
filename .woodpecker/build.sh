2acfe3180e547d802a9818788f76c5d4e180f072
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-08-05 11:54:33 +0200

    pipeline test

--- .woodpecker/build.sh
+++ .woodpecker/build.sh
@@ -1,13 +1,13 @@
-echo "DOCKER_PASSWORD" | docker login DOCKER_URL --username "DOCKER_USERNAME" --password-stdin
-echo "QUAY_PASSWORD" | docker login "QUAY_URL" --username "QUAY_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login "$DOCKER_URL" --username "$DOCKER_USERNAME" --password-stdin
+echo "$QUAY_PASSWORD" | docker login "$QUAY_URL" --username "$QUAY_USERNAME" --password-stdin
 cd s3cmd
 date=$(date +%Y%m%d)
 docker build . -f Dockerfile -t opencloudeu/s3cmd:v$date
 docker tag opencloudeu/s3cmd:v$date opencloudeu/s3cmd:v1
-docker tag opencloudeu/s3cmd:v$date DOCKER_URL/opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date DOCKER_URL/opencloudeu/s3cmd:v1
-docker tag opencloudeu/s3cmd:v$date QUAY_URL/opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date QUAY_URL/opencloudeu/s3cmd:v1
+docker tag opencloudeu/s3cmd:v$date $DOCKER_URL/opencloudeu/s3cmd:v$date
+docker tag opencloudeu/s3cmd:v$date $DOCKER_URL/opencloudeu/s3cmd:v1
+docker tag opencloudeu/s3cmd:v$date $QUAY_URL/opencloudeu/s3cmd:v$date
+docker tag opencloudeu/s3cmd:v$date $QUAY_URL/opencloudeu/s3cmd:v1
 
 docker push opencloudeu/s3cmd:v$date
 docker push opencloudeu/s3cmd:v1
