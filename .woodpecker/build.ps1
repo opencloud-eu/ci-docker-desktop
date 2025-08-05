7eccddae01805cd9c20dfe6ad2d25294a33242e1
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-08-05 12:27:01 +0200

    added manifests for multiplatform push

--- .woodpecker/build.ps1
+++ .woodpecker/build.ps1
@@ -1,17 +1,31 @@
-docker login $env:DOCKER_URL --username "$env:DOCKER_USERNAME" --password "$env:DOCKER_PASSWORD"
-docker login "$env:QUAY_URL" --username "$env:QUAY_USERNAME" --password "$env:QUAY_PASSWORD"
-cd s3cmd
 $date = get-date -format FileDate
-docker build . -f Dockerfile -t opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date opencloudeu/s3cmd:v1
-docker tag opencloudeu/s3cmd:v$date $env:DOCKER_URL/opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date $env:DOCKER_URL/opencloudeu/s3cmd:v1
-docker tag opencloudeu/s3cmd:v$date $env:QUAY_URL/opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date $env:QUAY_URL/opencloudeu/s3cmd:v1
+$DOCKER_URL = $env:DOCKER_URL
+$DOCKER_USERNAME = $env:DOCKER_USERNAME
+$DOCKER_PASSWORD = $env:DOCKER_PASSWORD
+$QUAY_URL = $env:QUAY_URL
+$QUAY_USERNAME = $env:QUAY_USERNAME
+$QUAY_PASSWORD = $env:QUAY_PASSWORD
 
-docker push opencloudeu/s3cmd:v$date
-docker push opencloudeu/s3cmd:v1
-docker push $env:DOCKER_URL/opencloudeu/s3cmd:v$date
-docker push $env:DOCKER_URL/opencloudeu/s3cmd:v1
-docker push $env:QUAY_URL/opencloudeu/s3cmd:v$date
-docker push $env:QUAY_URL/opencloudeu/s3cmd:v1
+docker login $DOCKER_URL --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+docker login "$QUAY_URL" --username "$QUAY_USERNAME" --password "$QUAY_PASSWORD"
+cd s3cmd
+docker build . -f Dockerfile -t opencloudeu/s3cmd:w-v$date
+docker tag opencloudeu/s3cmd:w-v$date opencloudeu/s3cmd:w-v1
+docker tag opencloudeu/s3cmd:w-v$date $DOCKER_URL/opencloudeu/s3cmd:w-v$date
+docker tag opencloudeu/s3cmd:w-v$date $DOCKER_URL/opencloudeu/s3cmd:w-v1
+docker tag opencloudeu/s3cmd:w-v$date $QUAY_URL/opencloudeu/s3cmd:w-v$date
+docker tag opencloudeu/s3cmd:w-v$date $QUAY_URL/opencloudeu/s3cmd:w-v1
+
+docker push opencloudeu/s3cmd:w-v$date
+docker push opencloudeu/s3cmd:w-v1
+docker push $DOCKER_URL/opencloudeu/s3cmd:w-v$date
+docker push $DOCKER_URL/opencloudeu/s3cmd:w-v1
+docker push $QUAY_URL/opencloudeu/s3cmd:w-v$date
+docker push $QUAY_URL/opencloudeu/s3cmd:w-v1
+
+docker manifest create -a opencloudeu/s3cmd:v1 opencloudeu/s3cmd:l-v1 opencloudeu/s3cmd:w-v1
+docker manifest create -a $DOCKER_URL/opencloudeu/s3cmd:v1 $DOCKER_URL/opencloudeu/s3cmd:l-v1 $DOCKER_URL/opencloudeu/s3cmd:w-v1
+docker manifest create -a $QUAY_URL/opencloudeu/s3cmd:v1 $QUAY_URL/opencloudeu/s3cmd:l-v1 $QUAY_URL/opencloudeu/s3cmd:w-v1
+docker manifest push opencloudeu/s3cmd:v1
+docker manifest push $DOCKER_URL/opencloudeu/s3cmd:v1
+docker manifest push $env:QUAY_URL/opencloudeu/s3cmd:v1
--- .woodpecker/build.sh
+++ .woodpecker/build.sh
@@ -1,17 +1,25 @@
+date=$(date +%Y%m%d)
+
 echo "$DOCKER_PASSWORD" | docker login "$DOCKER_URL" --username "$DOCKER_USERNAME" --password-stdin
 echo "$QUAY_PASSWORD" | docker login "$QUAY_URL" --username "$QUAY_USERNAME" --password-stdin
 cd s3cmd
-date=$(date +%Y%m%d)
-docker build . -f Dockerfile -t opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date opencloudeu/s3cmd:v1
-docker tag opencloudeu/s3cmd:v$date $DOCKER_URL/opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date $DOCKER_URL/opencloudeu/s3cmd:v1
-docker tag opencloudeu/s3cmd:v$date $QUAY_URL/opencloudeu/s3cmd:v$date
-docker tag opencloudeu/s3cmd:v$date $QUAY_URL/opencloudeu/s3cmd:v1
+docker build . -f Dockerfile -t opencloudeu/s3cmd:l-v$date
+docker tag opencloudeu/s3cmd:l-v$date opencloudeu/s3cmd:l-v1
+docker tag opencloudeu/s3cmd:l-v$date $DOCKER_URL/opencloudeu/s3cmd:l-v$date
+docker tag opencloudeu/s3cmd:l-v$date $DOCKER_URL/opencloudeu/s3cmd:l-v1
+docker tag opencloudeu/s3cmd:l-v$date $QUAY_URL/opencloudeu/s3cmd:l-v$date
+docker tag opencloudeu/s3cmd:l-v$date $QUAY_URL/opencloudeu/s3cmd:l-v1
 
-docker push opencloudeu/s3cmd:v$date
-docker push opencloudeu/s3cmd:v1
-docker push $DOCKER_URL/opencloudeu/s3cmd:v$date
-docker push $DOCKER_URL/opencloudeu/s3cmd:v1
-docker push $QUAY_URL/opencloudeu/s3cmd:v$date
-docker push $QUAY_URL/opencloudeu/s3cmd:v1
+docker push opencloudeu/s3cmd:l-v$date
+docker push opencloudeu/s3cmd:l-v1
+docker push $DOCKER_URL/opencloudeu/s3cmd:l-v$date
+docker push $DOCKER_URL/opencloudeu/s3cmd:l-v1
+docker push $QUAY_URL/opencloudeu/s3cmd:l-v$date
+docker push $QUAY_URL/opencloudeu/s3cmd:l-v1
+
+docker manifest create -a opencloudeu/s3cmd:v1 opencloudeu/s3cmd:l-v1 opencloudeu/s3cmd:w-v1
+docker manifest create -a $DOCKER_URL/opencloudeu/s3cmd:v1 $DOCKER_URL/opencloudeu/s3cmd:l-v1 $DOCKER_URL/opencloudeu/s3cmd:w-v1
+docker manifest create -a $QUAY_URL/opencloudeu/s3cmd:v1 $QUAY_URL/opencloudeu/s3cmd:l-v1 $QUAY_URL/opencloudeu/s3cmd:w-v1
+docker manifest push opencloudeu/s3cmd:v1
+docker manifest push $DOCKER_URL/opencloudeu/s3cmd:v1
+docker manifest push $QUAY_URL/opencloudeu/s3cmd:v1
