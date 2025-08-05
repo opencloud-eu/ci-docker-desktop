876f986ad157c40fc4a9bd06549e095095c1fcfd
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-08-05 11:45:02 +0200

    pipeline test

---
+++ .woodpecker/build.ps1
@@ -0,0 +1,17 @@
+echo "$env:DOCKER_PASSWORD" | docker login $env:DOCKER_URL --username "$env:DOCKER_USERNAME" --password-stdin
+echo "$env:QUAY_PASSWORD" | docker login "$env:QUAY_URL" --username "$env:QUAY_USERNAME" --password-stdin
+cd ../s3cmd
+$date = get-date -format FileDate
+docker build . -f Dockerfile -t opencloudeu/s3cmd:v$date
+docker tag opencloudeu/s3cmd:v$date opencloudeu/s3cmd:v1
+docker tag opencloudeu/s3cmd:v$date $env:DOCKER_URL/opencloudeu/s3cmd:v$date
+docker tag opencloudeu/s3cmd:v$date $env:DOCKER_URL/opencloudeu/s3cmd:v1
+docker tag opencloudeu/s3cmd:v$date $env:QUAY_URL/opencloudeu/s3cmd:v$date
+docker tag opencloudeu/s3cmd:v$date $env:QUAY_URL/opencloudeu/s3cmd:v1
+
+docker push opencloudeu/s3cmd:v$date
+docker push opencloudeu/s3cmd:v1
+docker push $env:DOCKER_URL/opencloudeu/s3cmd:v$date
+docker push $env:DOCKER_URL/opencloudeu/s3cmd:v1
+docker push $env:QUAY_URL/opencloudeu/s3cmd:v$date
+docker push $env:QUAY_URL/opencloudeu/s3cmd:v1
